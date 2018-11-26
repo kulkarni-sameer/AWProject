cdcc054125fb97dab6bd4e1442ad76b4c9e1229b
kulkarni-sameer <sameer43445@gmail.com>
2018-11-25 22:08:06 -0700

    Pronounce detected word instead of translated word

--- app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/ui/ActivityList/GermanActivity.java
+++ app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/ui/ActivityList/GermanActivity.java
@@ -149,7 +149,7 @@ public class GermanActivity extends Activity implements View.OnClickListener
         //mTTS.speak(text,TextToSpeech.QUEUE_FLUSH,null);
         System.out.println("Only the word " + detected_word);
         //speak the word detected
-        mTTS.speak(translated_word,TextToSpeech.QUEUE_FLUSH,null );
+        mTTS.speak(detected_word,TextToSpeech.QUEUE_FLUSH,null );
         //QueueFlush : current text gets cancelled to speak the new one
 
     }
