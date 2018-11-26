02e174bab5a3dcfa2c1052f18edcd68bae257a34
msinha7 <msinha7@asu.edu>
2018-11-26 00:47:29 -0700

    Added Instructions in respective language

--- app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/ui/ActivityList/ActivityLauncher.java
+++ app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/ui/ActivityList/ActivityLauncher.java
@@ -31,12 +31,8 @@ public class ActivityLauncher extends Activity implements OnClickListener
     private String mClassToLaunchPackage;
     private String username;
     private String ids;
-    private ListView mStepsListView;
-    private String[] steps=new String[]{
-           "Step 1: Point your phone camera on the word to translate it in your chosen language.",
-           "Step 2: After detection of the word,the application will take to a translated page .",
-           "Step 3: The translated page will have translated buttons too."
-   };
+    private TextView mInstructions;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -48,33 +44,11 @@ public class ActivityLauncher extends Activity implements OnClickListener
 
         setContentView(R.layout.initial_screen);
 
-        //setListAdapter(new ArrayAdapter<String>(this, R.layout.initial_screen, steps));
 
-        //ListView lv = getListView();
-        //lv.setTextFilterEnabled(true);
         mStartButton = (Button) findViewById(R.id.button_submit);
         mStartButton.setOnClickListener(this);
         mAboutTextTitle = (TextView) findViewById(R.id.about_text_title);
-        mStepsListView=(ListView) findViewById(R.id.steps);
-        List<String> stepsList=new ArrayList<>(Arrays.asList(steps));
-        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.initial_screen_text_view,stepsList){
-            @Override
-            public View getView (int position, View convertView, ViewGroup parent){
-                View view = super.getView(position,convertView,parent);
-
-                // Get the Layout Parameters for ListView Current Item View
-                ViewGroup.LayoutParams params = view.getLayoutParams();
-
-                // Set the height of the Item View
-                params.height = 200;  // earlier was 100
-                view.setLayoutParams(params);
-
-        return view;
-            }
-
-        };
-        mStepsListView.setAdapter(arrayAdapter);
-        mStepsListView.setDividerHeight(10);
+        mInstructions=(TextView) findViewById(R.id.steps);
         Bundle extras = getIntent().getExtras();
         System.out.println("***********************");
         System.out.println(extras.getString("par1"));
@@ -87,7 +61,52 @@ public class ActivityLauncher extends Activity implements OnClickListener
         ids = extras.getString("Selected_Level");
         username = extras.getString("Username");
        // mWelcomeTitle.setText("Bienvenue, " +username +"!");
-        mAboutTextTitle.setText("Instructions for using the Application, "+ username+"!");
+
+        if(ids.equals("Spanish")){
+            mAboutTextTitle.setText("Instrucciones:");
+            mInstructions.setText("1. Apunte la cámara de su teléfono a la palabra para traducirla al idioma elegido." + "\n"+
+                    "\n"+  "2. Después de la detección de la palabra, la aplicación lo llevará automáticamente a la página traducida." +"\n"+
+                    "\n"+    "3. La página traducida tendrá botones traducidos." +"\n"+
+                    "\n"+  "4. Haga clic en los botones para obtener resultados.");
+
+
+        }
+        else if(ids.equals("Portuguese")){
+            mAboutTextTitle.setText("Instruções:");
+            mInstructions.setText("1. Aponte a câmera do seu celular para a palavra para traduzi-la para o idioma escolhido." +"\n"+
+                    "\n"+     "2. Após a detecção da palavra, o aplicativo levará você para a página traduzida automaticamente." +"\n"+
+                    "\n"+  "3. A página traduzida terá botões traduzidos." +"\n"+
+                    "\n"+ "4. clique nos botões para obter resultados.");
+        }
+        else if(ids.equals("Italian")){
+            mAboutTextTitle.setText("Istruzioni:");
+            mInstructions.setText("1. Puntare la fotocamera del telefono sulla parola per tradurla nella lingua scelta." +"\n"+
+                    "\n"+  "2. Dopo il rilevamento della parola, l'applicazione ti porterà automaticamente alla pagina tradotta." +"\n"+
+                    "\n"+"3. La pagina tradotta avrà pulsanti tradotti." +"\n"+
+                    "\n"+"4. clicca sui pulsanti per ottenere risultati.");
+
+
+        }
+        else if(ids.equals("French")){
+            mAboutTextTitle.setText("Instructions:");
+            mInstructions.setText("1. Dirigez l'appareil photo de votre téléphone vers le mot pour le traduire dans la langue de votre choix."
+                    +"\n"+"\n"+
+                    "2. Après détection du mot, l'application vous mènera automatiquement à la page traduite." +"\n"+"\n"+
+                    "3. La page traduite aura des boutons traduits." +"\n"+"\n"+
+                    "4. cliquez sur les boutons pour obtenir des résultats.");
+
+        }
+        else if(ids.equals("German")){
+            mAboutTextTitle.setText("Anweisungen:");
+            mInstructions.setText("1. Richten Sie Ihre Telefonkamera auf das Wort, um es in die von Ihnen gewählte Sprache zu übersetzen." +"\n"+
+                    "\n"+      "2. Nachdem das Wort erkannt wurde, führt Sie die Anwendung automatisch zur übersetzten Seite." +"\n"+
+                    "\n"+     "3.Die übersetzte Seite enthält Schaltflächen." +"\n"+
+                    "\n"+"4. Klicken Sie auf die Schaltflächen, um Ergebnisse zu erhalten");
+
+        }
+        else {
+            //pass
+        }
 
     }
 
